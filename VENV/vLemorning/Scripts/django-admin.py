f342050c4c6e3b3c4977d66f61b2ca67e9d29f9b
youdongkwan <dkyou7@naver.com>
2020-05-14 23:54:45 +0900

    [dongkwan]_first commit

---
+++ VENV/vLemorning/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dkyou\onedrive\바탕 화면\6th-lemorning-django\venv\vlemorning\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
