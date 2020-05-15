0259da1498aa2acee477fcb09360a4cee3867480
youdongkwan <dkyou7@naver.com>
2020-05-15 14:14:32 +0900

    renew folder

--- VENV/vLemorning/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\dkyou\onedrive\바탕 화면\6th-lemorning-django\venv\vlemorning\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
