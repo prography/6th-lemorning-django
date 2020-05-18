ac47d2babeed507e091e58c9e9dd5f05d3a1fbf3
이창섭 <ventulus95@gmail.com>
2020-05-18 23:09:10 +0900

    [창섭] audio 업로드 및 재생 구현 - 파일 업로드중 validator를 통해 mp3만 들어갈 수 있게 구현.(차후 변경할 것) - 파일 업로드 후 재생 플레이어에 재생 구현.

--- board/models.py
+++ board/models.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.core.validators import FileExtensionValidator
 
 
 # Create your models here.
 class Board(models.Model):
     title = models.CharField(max_length=300)
-    alarm = models.FileField(blank=True, upload_to="alarm/%Y/%m/%d")
+    alarm = models.FileField(blank=True, upload_to="alarm/%Y/%m/%d", validators=[FileExtensionValidator(allowed_extensions=['mp3'])])
     create_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
