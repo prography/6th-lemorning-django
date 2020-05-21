601927bb10cc404d17d5c14cf8ef45418b2a3792
youdongkwan <dkyou7@naver.com>
2020-05-21 17:49:45 +0900

    카테고리별 게시글 생성 및 적용

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,6 @@ app_name = 'shop'
 
 urlpatterns = [
     path('',product_in_category,name='product_all'),
-    path('/<slug:category_slug>/',product_in_category,name='product_in_category'),
-    path('/<int:id>/<product_slug>/',product_detail,name='product_detail')
+    path('<slug:category_slug>/',product_in_category,name='product_in_category'),
+    path('<int:id>/<product_slug>/',product_detail,name='product_detail')
 ]
